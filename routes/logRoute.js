e6eae2b94758a2450f2cda7f6c2bfbea7b3ec2f8
Matheus Luiz <matheus.felicori@hotmail.com>
2024-06-20 06:10:24 -0300

    Atualização 2.3

--- routes/logRoute.js
+++ routes/logRoute.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const logController = require('../controllers/logController');
 
-// Rota para obter todos os logs
+
 router.get('/', logController.getAllLogs);
 
-// Rota para obter logs por tabela afetada
+
 router.get('/:tabela_afetada', logController.getLogsByTable);
 
 module.exports = router;
